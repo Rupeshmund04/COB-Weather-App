b52757da1019d9666897d033c7d6120f6e641e4c
Rupeshmund04 <115936873+Rupeshmund04@users.noreply.github.com>
2025-08-17 14:24:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const cityName = document.getElementById('cityName');
+const cloud_pct = document.getElementById('cloud_pct');
+const temp = document.getElementById('temp');
+const feels_like = document.getElementById('feels_like');
+const humidity = document.getElementById('humidity');
+const min_temp = document.getElementById('min_temp');
+const max_temp = document.getElementById('max_temp');
+const wind_speed = document.getElementById('wind_speed');
+const wind_degrees = document.getElementById('wind_degrees');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+// const weather_desc = document.getElementById('weather_desc'); // Optional
+const submit = document.getElementById('submit');
+const city = document.getElementById('city');
+
 
 const apiKey = "7271b6627c1dc186e7cdcaa739be3c15";
 
@@ -21,7 +36,7 @@ const getWeather = (city) => {
         wind_speed.innerHTML = response.wind.speed;
         wind_degrees.innerHTML = response.wind.deg;
 
-        weather_desc.innerHTML = response.weather[0].description;
+        // weather_desc.innerHTML = response.weather[0].description;
         
         const sunriseDate = new Date(response.sys.sunrise * 1000);
         sunrise.innerHTML = sunriseDate.toLocaleTimeString();
@@ -61,5 +76,6 @@ function validateform() {
 }
 
     
+
 
 
